0115b04649844209b1bae8bbbb23160c9b0994e2
mariano-shem <shemmariano56@gmail.com>
2025-06-02 21:26:30 +0800

    modularizedlogic

---
+++ related_projects/8-weather/src/js/get_data.js
@@ -0,0 +1,107 @@
+const $city_name = document.querySelector("#city_name");
+const $country = document.querySelector("#country");
+const $condition = document.querySelector("#condition");
+const $tempc_today = document.querySelector("#tempc_today");
+const $icon_today = document.querySelector("#icon_today");
+const $forecast_ul = document.querySelector("#forecast_ul");
+
+export async function getData(input_city = "Manila") {
+  try {
+    const weather_response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${
+        import.meta.env.VITE_API_KEY
+      }&q=${input_city}&days=4`
+    );
+    const weather_data = await weather_response.json();
+    // Destructuring data--
+    const {
+      location: { name: city, country },
+      current: {
+        temp_c,
+        condition: { text, icon },
+      },
+      forecast: {
+        forecastday: [
+          ,
+          {
+            date: date2,
+            day: {
+              avgtemp_c: day2_temp,
+              condition: { icon: day2_icon },
+            },
+          },
+          {
+            date: date3,
+            day: {
+              avgtemp_c: day3_temp,
+              condition: { icon: day3_icon },
+            },
+          },
+          {
+            date: date4,
+            day: {
+              avgtemp_c: day4_temp,
+              condition: { icon: day4_icon },
+            },
+          },
+        ],
+      },
+    } = weather_data;
+    displayWeather(city, country, icon, temp_c, text);
+    displayForecast(
+      { date: date2, icon: day2_icon, temp: day2_temp },
+      { date: date3, icon: day3_icon, temp: day3_temp },
+      { date: date4, icon: day4_icon, temp: day4_temp }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function displayWeather(...weatherdata) {
+  const [city, country, icon, temp_c, condition] = weatherdata;
+  try {
+    await new Promise((resolve) => {
+      $icon_today.src = icon;
+      $icon_today.onload = () => resolve();
+    });
+    $city_name.textContent = city;
+    $country.textContent = country;
+    $tempc_today.textContent = `${temp_c.toFixed(0)}°C`;
+    $condition.textContent = condition;
+  } catch {}
+}
+function displayForecast(...forecastdata) {
+  const items = $forecast_ul.querySelectorAll("li");
+
+  forecastdata.forEach(({ date, icon, temp }, index) => {
+    const li = items[index];
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    const month = parseInt(date.slice(5, 7), 10);
+    const day = date.slice(8, 10);
+    console.log(month, day);
+    li.querySelector("img").src = icon;
+    li.querySelectorAll("span")[0].textContent = `${months[month - 1]} ${day}`;
+    li.querySelectorAll("span")[1].textContent = `${temp.toFixed(0)}°C`;
+  });
+
+  /**
+   * Result should display:
+   * <li>[icon2][temp2]</li>
+   * <li>[icon3][temp3]</li>
+   * <li>[icon4][temp4]</li>
+   */
+}
--- related_projects/8-weather/src/js/main.js
+++ related_projects/8-weather/src/js/main.js
@@ -1,22 +1,32 @@
+import { getData } from "./get_data.js"
+
 const $form = document.querySelector("#form_id");
 const $input = $form.querySelector("#city_input");
 const $today = document.querySelector("#today");
 const $city_name = document.querySelector("#city_name");
 const $country = document.querySelector("#country");
-const $condition = document.querySelector("#condition");
-const $tempc_today = document.querySelector("#tempc_today");
-const $icon_today = document.querySelector("#icon_today");
-const $forecast_ul = document.querySelector("#forecast_ul");
 
 const API_KEY = "";
 
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
   let city = $input.value;
-  getWeatherData(city);
+  queryCity(city);
   $input.value = "";
 });
+async function queryCity(input_city) {
+  try {
+    // Loading screen
+    $city_name.textContent = "Finding city...";
 
+    await getData(input_city);
+  } catch (err) {
+    console.error(err);
+    $country.textContent = "Not Found.";
+  } finally {
+    // $load_screen.textContent = "";
+  }
+}
 function dateToday() {
   const date = new Date();
   let day = date.getDay();
@@ -37,94 +47,4 @@ function dateToday() {
   $today.textContent = `${days_arr[day]}, ${hour}:${minute}:${second}`;
 }
 setInterval(dateToday, 1000);
-
-async function getWeatherData(input_city) {
-  try {
-    // Loading screen
-    $city_name.textContent = "Finding city...";
-
-    const weather_response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${
-        import.meta.env.VITE_API_KEY
-      }&q=${input_city}&days=4`
-    );
-    const weather_data = await weather_response.json();
-
-    // Destructuring data--
-    const {
-      location: { name: city, country },
-      current: {
-        temp_c,
-        condition: { text, icon },
-      },
-      forecast: {
-        forecastday: [
-          ,
-          {
-            day: {
-              avgtemp_c: day2_temp,
-              condition: { icon: day2_icon },
-            },
-          },
-          {
-            day: {
-              avgtemp_c: day3_temp,
-              condition: { icon: day3_icon },
-            },
-          },
-          {
-            day: {
-              avgtemp_c: day4_temp,
-              condition: { icon: day4_icon },
-            },
-          },
-        ],
-      },
-    } = weather_data;
-
-    displayWeatherToday(city, country, icon, temp_c, text);
-    displayForecast(
-      day2_icon,
-      day3_icon,
-      day4_icon,
-      day2_temp,
-      day3_temp,
-      day4_temp
-    );
-
-    // Temporary console logging
-    console.log(weather_data);
-    // console.log(weather_data);
-  } catch (err) {
-    console.log(err);
-    $country.textContent = "Not Found.";
-  } finally {
-    // $load_screen.textContent = "";
-  }
-}
-
-async function displayWeatherToday(city, country, icon, temp_c, condition) {
-  try {
-    await new Promise((resolve) => {
-      $icon_today.src = icon;
-      $icon_today.onload = () => resolve();
-    });
-    $city_name.textContent = city;
-    $country.textContent = country;
-    $tempc_today.textContent = `${temp_c.toFixed(0)}°C`;
-    $condition.textContent = condition;
-  } catch {}
-}
-
-function displayForecast(icon2, icon3, icon4, temp2, temp3, temp4) {
-  let items = $forecast_ul.querySelectorAll("li");
-  let icons = [icon2, icon3, icon4];
-  let temps = [temp2, temp3, temp4];
-
-  for (let li = 0; li < items.length; li++) {
-    items[li].querySelector("img").src = icons[li];
-    items[li].querySelectorAll("span")[1].textContent = `${temps[li].toFixed(
-      0
-    )}°C`;
-  }
-}
+getData();
